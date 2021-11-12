63a56906f4ded8d219d0180f8fedf3f1520c2750
Hernan Condori <hcondori@elementum.com>
2021-11-11 22:19:27 -0300

    API with express

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+app.get('/mensaje', (req, res) => {
+  res.send('HOla NOdeJS desde heroku!!!');
+});
+
+const PORT = process.env.PORT || 8080;
+
+const  server = app.listen(PORT, () => console.log(`API EXPRESS run in ${PORT}`))
+
+server.on('error', (error) => console.log('Server Error: ', error));
